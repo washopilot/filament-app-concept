0864786e18a6d41f56298c9a9dad0c5fe144a35b
Fernando Chicaiza <washopilot@yahoo.com>
2024-07-24 16:37:07 +0000

    blade components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,9 @@
     <x-slot:title>
         Este es un título
     </x-slot>
+    <x-hero />
+    <x-section-one />
+    <x-section-two />
+    <x-section-pricing />
+    <x-section-call />
 </x-layout>
